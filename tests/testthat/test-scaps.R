242642b7e93853a2f77111e45346438163f96a20
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2018-07-30 16:21:56 +1000

    add another test to `scaps()` for handling numeric input and edit documentation

--- tests/testthat/test-scaps.R
+++ tests/testthat/test-scaps.R
@@ -8,3 +8,11 @@ test_that("multiple string-input produces output of length greater than 1L", {
 test_that("unidentified object input returns error", {
   expect_error(scaps(x), "^object.+?not found$", perl = TRUE)
 })
+
+string <- c(1L, 5L)
+test_that("numeric object input is rendered as character", {
+  for (i in seq_along(string)) {
+    expect_match(scaps(string)[i], paste(">", string[i], "<", sep = ""), all =TRUE)
+    expect_type(scaps(string)[i], "character")
+  }
+})
