d254f9ae4090c0f39375eb0d86602081673c5e43
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2018-07-31 16:23:05 +1000

    edit spelling error in `scaps_italics()`

--- R/scaps_italics.R
+++ R/scaps_italics.R
@@ -3,7 +3,7 @@
 #' @description This is a wrapper to put HTML-tag around character string input to be rendered as \emph{italicised} small capital in the output document of an R Markdown file.
 #' @param x A character string (e.g. a word or phrase).
 #'
-#' @return Italicised, small captial character string.
+#' @return Italicised, small capital character string.
 #' @export
 #'
 #' @examples
